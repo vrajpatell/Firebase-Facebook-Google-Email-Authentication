30336e4596b72fff4aa70b9688563efc004c992a
vrajpatell <51841184+vrajpatell@users.noreply.github.com>
2020-06-26 15:38:25 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,13 +10,13 @@ Vue.config.productionTip = false
 //Firebase config
 const firebaseConfig = {
   // firebase initialization 
-  apiKey: 'AIzaSyA0qVgt62Wn3RBsyAeaZTAZ6OE3_nVDV5w',
-  authDomain: "oc-boardgames-85ef0.firebaseio.com",
-  databaseURL: "https://oc-boardgames-85ef0.firebaseio.com",
-  projectId: "oc-boardgames-85ef0",
-  storageBucket: "gs://oc-boardgames-85ef0.appspot.com",
-  messagingSenderId: "303496283538",
-  appId: "1:303496283538:web:3f69e89774df3fccc56d4b"
+  apiKey: ' ',
+  authDomain: " ",
+  databaseURL: " ",
+  projectId: "  ",
+  storageBucket: "   ",
+  messagingSenderId: " ",
+  appId: "   "
 };
 
 firebase.initializeApp(firebaseConfig);
